e84b624c7b395b3aea9f8ed4870872fe868a3d5d
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-06-05 16:38:04 -0500

    subiendo cambios en los path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
-//const routerApi = require('./users.router');
+const categoriesRouter = require('./categories.router');
+const routerApi = require('./users.router');
 
 function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
   app.use('/products', productsRouter);
-  //app.use('/users', usersRouter);
+  app.use('/categories', categoriesRouter);
+  app.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
---
+++ routes/users.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.router();
+
+router.get('/', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+});
