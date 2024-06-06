d58d0dfbd278cddb5c1c44f614296d735779720a
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-06-05 20:38:10 -0500

    subiendo cambios en el post

--- routes/products.router.js
+++ routes/products.router.js
@@ -3,7 +3,7 @@ const faker = require('faker');
 
 const router = express.Router();
 
-router.get('/products', (req, res) => {
+router.get('/', (req, res) => {
   const products = [];
   const { size } = req.query;
   const limit = size || 10;
@@ -27,6 +27,32 @@ router.get('/:id', (req, res) => {
     id,
     name: 'Product 2',
     price: 2000
+  });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
