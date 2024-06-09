9c00e18574c7152290e325b0ede171bf93b2ff5e
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-06-08 19:31:57 -0500

    manejando errores en los middlewares

--- services/product.service.js
+++ services/product.service.js
@@ -1,8 +1,9 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
-  constructor() {
+  constructor(){
     this.products = [];
     this.generate();
   }
@@ -19,20 +20,51 @@ class ProductsService {
     }
   }
 
-  create() {
-
+  async create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   find() {
-    return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    })
   }
 
-  findOne(id) {
-    return this.products.find(item => item.id === id);
+  async findOne(id) {
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound('product not found');
+    }
+    return product;
   }
 
-  update() {
+  async update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
 
+  async delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 
 }
